4b18a912e05e083a9cc7918c646a6401a86a1b73
GustahYakuza <129031822+GustahYakuza@users.noreply.github.com>
2023-11-23 12:47:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-// ...
+let compliments = [
+    "Você é incrível!",
+    "Seu sorriso ilumina meu dia!",
+    "Ninguém é tão especial quanto você!",
+    "Sua bondade é contagiante!",
+    "O mundo é melhor com você nele!",
+    "Você é a razão do meu sorriso!",
+    "Sua inteligência me inspira!",
+    "Seu coração é tão generoso!",
+    "Cada dia ao seu lado é um presente!",
+    "Você é o amor da minha vida!"
+];
 
-// Adicione essa função para carregar os elogios do arquivo JSON
-function loadCompliments() {
-    fetch('compliments.json')
-        .then(response => response.json())
-        .then(data => {
-            compliments = data;
-            showRandomCompliment();
-        })
-        .catch(error => console.error('Erro ao carregar os elogios:', error));
+function showRandomCompliment() {
+    const randomIndex = Math.floor(Math.random() * compliments.length);
+    const compliment = compliments[randomIndex];
+
+    const complimentElement = document.querySelector('.compliment-text');
+    complimentElement.textContent = compliment;
 }
 
-// Carrega os elogios ao iniciar o script
-loadCompliments();
+document.getElementById('changeCompliment').addEventListener('click', showRandomCompliment);
+
+// Mostra um elogio ao carregar a página
+showRandomCompliment();
